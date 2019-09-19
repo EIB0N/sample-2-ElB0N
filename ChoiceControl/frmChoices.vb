204b7480387fa24789aa8d3f4d553591074363ca
Ronald G <rgonz428@fiu.edu.com>
2019-09-19 19:14:31 -0400

    Lecture 3 - Part 1

---
+++ ChoiceControl/frmChoices.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmChoices
+
+    Private Sub LoadComboBoxChoices()
+        cboSaleItems.Items.Add("Long sleeve shirt")
+        cboSaleItems.Items.Add("Short sleeve shirt")
+        cboSaleItems.Items.Add("Lightweight jacket")
+    End Sub
+
+    Private Sub frmChoices_Load(sender As Object, e As EventArgs) Handles Me.Load
+        LoadComboBoxChoices()
+
+    End Sub
+End Class
