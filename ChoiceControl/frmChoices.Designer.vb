829f7264cc1786bf1065557487f0ac0b41399eb2
Ronald G <rgonz428@fiu.edu.com>
2019-09-19 22:32:45 -0400

    Lecture 3 - Part 4

--- ChoiceControl/frmChoices.Designer.vb
+++ ChoiceControl/frmChoices.Designer.vb
@@ -33,13 +33,21 @@ Partial Class frmChoices
         Me.redGreen = New System.Windows.Forms.RadioButton()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
-        Me.redGold = New System.Windows.Forms.RadioButton()
-        Me.redSilver = New System.Windows.Forms.RadioButton()
         Me.redBlack = New System.Windows.Forms.RadioButton()
+        Me.redSilver = New System.Windows.Forms.RadioButton()
+        Me.redGold = New System.Windows.Forms.RadioButton()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
-        Me.chkExpress = New System.Windows.Forms.CheckBox()
-        Me.chkSaturday = New System.Windows.Forms.CheckBox()
         Me.chkResidential = New System.Windows.Forms.CheckBox()
+        Me.chkSaturday = New System.Windows.Forms.CheckBox()
+        Me.chkExpress = New System.Windows.Forms.CheckBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.dtmApproval = New System.Windows.Forms.DateTimePicker()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.MonthCalendar1 = New System.Windows.Forms.MonthCalendar()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.mskZip = New System.Windows.Forms.MaskedTextBox()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
@@ -155,16 +163,16 @@ Partial Class frmChoices
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "More Radio Buttons - Trim"
         '
-        'redGold
+        'redBlack
         '
-        Me.redGold.AutoSize = True
-        Me.redGold.Location = New System.Drawing.Point(18, 31)
-        Me.redGold.Name = "redGold"
-        Me.redGold.Size = New System.Drawing.Size(47, 17)
-        Me.redGold.TabIndex = 0
-        Me.redGold.TabStop = True
-        Me.redGold.Text = "Gold"
-        Me.redGold.UseVisualStyleBackColor = True
+        Me.redBlack.AutoSize = True
+        Me.redBlack.Location = New System.Drawing.Point(18, 78)
+        Me.redBlack.Name = "redBlack"
+        Me.redBlack.Size = New System.Drawing.Size(52, 17)
+        Me.redBlack.TabIndex = 2
+        Me.redBlack.TabStop = True
+        Me.redBlack.Text = "Black"
+        Me.redBlack.UseVisualStyleBackColor = True
         '
         'redSilver
         '
@@ -177,16 +185,16 @@ Partial Class frmChoices
         Me.redSilver.Text = "Silver"
         Me.redSilver.UseVisualStyleBackColor = True
         '
-        'redBlack
+        'redGold
         '
-        Me.redBlack.AutoSize = True
-        Me.redBlack.Location = New System.Drawing.Point(18, 78)
-        Me.redBlack.Name = "redBlack"
-        Me.redBlack.Size = New System.Drawing.Size(52, 17)
-        Me.redBlack.TabIndex = 2
-        Me.redBlack.TabStop = True
-        Me.redBlack.Text = "Black"
-        Me.redBlack.UseVisualStyleBackColor = True
+        Me.redGold.AutoSize = True
+        Me.redGold.Location = New System.Drawing.Point(18, 31)
+        Me.redGold.Name = "redGold"
+        Me.redGold.Size = New System.Drawing.Size(47, 17)
+        Me.redGold.TabIndex = 0
+        Me.redGold.TabStop = True
+        Me.redGold.Text = "Gold"
+        Me.redGold.UseVisualStyleBackColor = True
         '
         'GroupBox3
         '
@@ -200,15 +208,15 @@ Partial Class frmChoices
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "Check Boxes - Delivery"
         '
-        'chkExpress
+        'chkResidential
         '
-        Me.chkExpress.AutoSize = True
-        Me.chkExpress.Location = New System.Drawing.Point(16, 22)
-        Me.chkExpress.Name = "chkExpress"
-        Me.chkExpress.Size = New System.Drawing.Size(104, 17)
-        Me.chkExpress.TabIndex = 0
-        Me.chkExpress.Text = "Express Delivery"
-        Me.chkExpress.UseVisualStyleBackColor = True
+        Me.chkResidential.AutoSize = True
+        Me.chkResidential.Location = New System.Drawing.Point(16, 68)
+        Me.chkResidential.Name = "chkResidential"
+        Me.chkResidential.Size = New System.Drawing.Size(119, 17)
+        Me.chkResidential.TabIndex = 2
+        Me.chkResidential.Text = "Residential Address"
+        Me.chkResidential.UseVisualStyleBackColor = True
         '
         'chkSaturday
         '
@@ -220,21 +228,94 @@ Partial Class frmChoices
         Me.chkSaturday.Text = "Saturday Delivery"
         Me.chkSaturday.UseVisualStyleBackColor = True
         '
-        'chkResidential
+        'chkExpress
         '
-        Me.chkResidential.AutoSize = True
-        Me.chkResidential.Location = New System.Drawing.Point(16, 68)
-        Me.chkResidential.Name = "chkResidential"
-        Me.chkResidential.Size = New System.Drawing.Size(119, 17)
-        Me.chkResidential.TabIndex = 2
-        Me.chkResidential.Text = "Residential Address"
-        Me.chkResidential.UseVisualStyleBackColor = True
+        Me.chkExpress.AutoSize = True
+        Me.chkExpress.Location = New System.Drawing.Point(16, 22)
+        Me.chkExpress.Name = "chkExpress"
+        Me.chkExpress.Size = New System.Drawing.Size(104, 17)
+        Me.chkExpress.TabIndex = 0
+        Me.chkExpress.Text = "Express Delivery"
+        Me.chkExpress.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(391, 39)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(172, 13)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Date , Time Picker - Approval Date"
+        '
+        'dtmApproval
+        '
+        Me.dtmApproval.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.dtmApproval.Location = New System.Drawing.Point(394, 56)
+        Me.dtmApproval.Name = "dtmApproval"
+        Me.dtmApproval.Size = New System.Drawing.Size(200, 20)
+        Me.dtmApproval.TabIndex = 14
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(391, 93)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(168, 13)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Month Calander - Sale Date range"
+        '
+        'MonthCalendar1
+        '
+        Me.MonthCalendar1.Location = New System.Drawing.Point(394, 114)
+        Me.MonthCalendar1.Name = "MonthCalendar1"
+        Me.MonthCalendar1.TabIndex = 16
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(395, 285)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(147, 13)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "TextBox - Manager Id number"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(393, 313)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(166, 20)
+        Me.TextBox1.TabIndex = 18
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(390, 343)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(161, 13)
+        Me.Label7.TabIndex = 19
+        Me.Label7.Text = "MaskedTextBox- Store Zip Code"
+        '
+        'mskZip
+        '
+        Me.mskZip.Location = New System.Drawing.Point(397, 367)
+        Me.mskZip.Mask = "00000-9999"
+        Me.mskZip.Name = "mskZip"
+        Me.mskZip.Size = New System.Drawing.Size(162, 20)
+        Me.mskZip.TabIndex = 20
         '
         'frmChoices
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 690)
+        Me.Controls.Add(Me.mskZip)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.MonthCalendar1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.dtmApproval)
+        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
@@ -275,4 +356,12 @@ Partial Class frmChoices
     Friend WithEvents chkResidential As CheckBox
     Friend WithEvents chkSaturday As CheckBox
     Friend WithEvents chkExpress As CheckBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents dtmApproval As DateTimePicker
+    Friend WithEvents Label5 As Label
+    Friend WithEvents MonthCalendar1 As MonthCalendar
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents mskZip As MaskedTextBox
 End Class
